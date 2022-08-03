8f52ab81026d02b10af48a3cb7fbc26ba729566d
Mark Stankus <mstankus@gmail.com>
2022-08-03 13:05:39 -0700

    Added two more examples

--- print_backspace_characters/src/main.rs
+++ print_backspace_characters/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
   println!("This \x08does \x08not \x08have \x08any \x08spaces.");
+
+  print!("Before");
+  print!("\x08\x08\x08\x08\x08\x08");
+  println!("After 1");
+
+  print!("Before");
+  for _i in 0.."Before".chars().count() {
+    print!("{}",'\x08');
+  }
+  println!("After 2");
 }
